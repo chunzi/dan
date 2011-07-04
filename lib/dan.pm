b3f42aa76b1611d7c4d9d3aaa7fbcdbcc6967206
chunzi <chunzi@gmail.com>
2011-07-04 16:47:05 +0800

    show date on the post page

--- lib/dan.pm
+++ lib/dan.pm
@@ -15,25 +15,7 @@ my $post_dir = dir("$FindBin::Bin/../posts");
 
 get '/' => sub {
     my @post_files = grep { -f and $_->basename =~ /\.md/ } $post_dir->children;
-    my @posts = map {
-        my $lines = read_file("$_");
-        my ( $header, $body ) = split "\n\n", $lines, 2;
-        my $meta = {};
-        map { 
-            my ( $key, $value ) = split /\:/, $_, 2;
-            $meta->{$key} = $value;
-        } split "\n", $header;
-        my $uri = $_->basename;
-        $uri =~ s/\.md$/\.html/;
-
-        {
-            title => $meta->{'title'},
-            slug => $meta->{'slug'},
-            date => date($meta->{'date'}),
-            uri => $uri,
-        }
-    } @post_files;
-
+    my @posts = map { file2post( $_ ) } @post_files;
     var posts => \@posts;
     template 'index', vars;
 };
@@ -42,11 +24,31 @@ get '/post/:name' => sub {
     my $name = params->{'name'};
     $name =~ s/\.html/\.md/g;
     my $file = $post_dir->file( $name );
-    my $lines = read_file("$file");
-    my ( $header, $body ) = split "\n\n", $lines, 2;
-    my $content = markdown( $body );
-    var html => $content;
+    my $post = file2post( $file );
+    var post => $post;
     template 'post', vars;
 };
+
+sub file2post {
+    my $file = shift;
+    my $lines = read_file("$file");
+    my ( $header, $body ) = split "\n\n", $lines, 2;
+    my $meta = {};
+    map { 
+        my ( $key, $value ) = split /\:/, $_, 2;
+        $meta->{$key} = $value;
+    } split "\n", $header;
+    my $uri = $file->basename;
+    $uri =~ s/\.md$/\.html/;
+
+    my $post = {
+        title => $meta->{'title'},
+        slug => $meta->{'slug'},
+        date => date($meta->{'date'}),
+        uri => $uri,
+        html => markdown( $body || '' ),
+    };
+    return $post;
+}
 
 true;
