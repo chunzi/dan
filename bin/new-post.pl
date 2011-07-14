de722f40088da5367d02b8ddbd12c16d1067051a
chunzi <chunzi@gmail.com>
2011-07-14 16:19:52 +0800

    using Byword as the default editor

--- bin/new-post.pl
+++ bin/new-post.pl
@@ -9,15 +9,15 @@ use Class::Date qw/ date -DateParse /;
 our $dir = '/Users/chunzi/github/chunzi.dan/posts'; 
 
 my $today = date( time );
-my $filename = sprintf "%s/%s-slug.md", $dir, $today->strftime( '%Y-%m-%d' );
+my $filename = sprintf "%s/%s-new.md", $dir, $today->strftime( '%Y-%m-%d' );
 
 unless ( -f $filename ){
-    my $text = sprintf "title:untitled\nslug:slug\ndate:%s\n\n# ", $today;
+    my $text = sprintf "title: \nslug: \ndate: %s\n\n# ", $today;
     write_file( $filename, $text );
     printf STDERR "Created %s\n", $filename;
 }
 
-system( 'open', $filename );
+system( 'open', '-a', 'Byword', $filename );
 printf STDERR "Opened %s\n", $filename;
 
 exit;
